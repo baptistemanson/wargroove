e33a8f4fd87aa0787cef286b7d7963fdc2c6f22f
baptistemanson <baptiste.manson@inovia.fr>
2019-02-09 10:01:25 -0800

    Updates

---
+++ precache-manifest.981884d9895ae2a7e585d36179421cee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6750423936c03731b559",
+    "url": "/wargroove/static/js/runtime~main.67504239.js"
+  },
+  {
+    "revision": "5d6d4373b1593cf3b480",
+    "url": "/wargroove/static/js/main.5d6d4373.chunk.js"
+  },
+  {
+    "revision": "5921907360e97f611391",
+    "url": "/wargroove/static/js/1.59219073.chunk.js"
+  },
+  {
+    "revision": "5d6d4373b1593cf3b480",
+    "url": "/wargroove/static/css/main.79e3bda4.chunk.css"
+  },
+  {
+    "revision": "5921907360e97f611391",
+    "url": "/wargroove/static/css/1.24b2daa5.chunk.css"
+  },
+  {
+    "revision": "8ccbc67cae175471d256eb07c0ccc0cc",
+    "url": "/wargroove/index.html"
+  }
+];
